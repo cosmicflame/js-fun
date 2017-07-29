a56a2b2c89650f6f8b3737be68cc133a5fa4910f
Brian Beckett <brian@worthlesscheese.com>
2017-07-29 17:21:03 +0100

    Thoughts

--- euler/problem1.js
+++ euler/problem1.js
@@ -2,6 +2,10 @@
 
 const testData = [];
 
+
+// In hindsight, this is really ugly
+// Why create a dataset only to throw half of it away?
+// I could - and should - have been filtering in the initial loop
 for (let i = 1; i < 1000; i++) {
   testData.push(i);
 }
